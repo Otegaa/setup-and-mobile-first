910c5aa2c6536d39ad09ba0803f4581b657eac82
otegaa <ekiugbotega@gmail.com>
2023-03-06 18:11:05 +0100

    Did hamburger popup menu

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const testing = function () {
-  const x = 6;
-  return x;
-};
-testing();
+const btn = document.querySelector('.info-image');
+const showHamburger = document.querySelector('.show-hamburger');
+const menuLinks = document.querySelectorAll('.menu-links');
+const closeBtn = document.querySelector('.close-cross');
+
+function checkHamburger() {
+  showHamburger.classList.remove('hidden');
+}
+
+btn.addEventListener('click', checkHamburger);
+
+function closeButton() {
+  showHamburger.classList.add('hidden');
+}
+
+closeBtn.addEventListener('click', closeButton);
+
+menuLinks.forEach((link) => link.addEventListener('click', () => {
+  showHamburger.classList.add('hidden');
+}));
